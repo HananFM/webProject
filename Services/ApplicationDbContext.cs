c6a23910826f124b85f0c6a9b09122494f5ef6fa
daniaXmasri <132768928+daniaXmasri@users.noreply.github.com>
2024-12-11 17:15:04 +0300

    Add files via upload
    login sayfasina baslamak

---
+++ Services/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WebProje3.Models;
+
+namespace WebProje3.Services
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options) { }
+    }
+}
