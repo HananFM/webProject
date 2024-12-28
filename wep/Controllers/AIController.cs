15171ccb9543ed1647af1aeafffe5e92ad135aa9
Hanan <hanan.hf2002@hotmail.com>
2024-12-28 15:38:59 +0300

    Dania 25,27,28/12

---
+++ wep/Controllers/AIController.cs
@@ -0,0 +1,159 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Net;
+using System.Text;
+
+namespace wep.Controllers
+{
+    public class AIController : Controller
+    {
+        private const string ApiUrl = "https://api.lightxeditor.com/external/api/v1/hairstyle";
+        private const string statusApiUrl = "https://api.lightxeditor.com/external/api/v1/order-status";
+        private const string ApiKey = "383fb4ea0b0b4660bea9a2c0a20b2b01_10cb6a1ce9d549e088f1520d45899bb5_andoraitools";
+        HttpClient client = new HttpClient();
+        string urlBase = "https://masri.uk/dania/uploaded";
+
+        public AIController()
+        {
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.TryAddWithoutValidation("Content-Type", "application/json");
+            client.DefaultRequestHeaders.Add("x-api-key", ApiKey);
+        }
+        [HttpGet]
+        public IActionResult UploadPhoto()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UploadPhoto(IFormFile photo, string prompt)
+        {
+            if (photo == null || photo.Length == 0)
+            {
+                return BadRequest("No file uploaded.");
+            }
+
+            // Save the photo to a temporary location
+            var filePath = Path.Combine("wwwroot/uploads", photo.FileName);
+            using (var stream = new FileStream(filePath, FileMode.Create))
+            {
+                await photo.CopyToAsync(stream);
+            }
+
+            if (Ftp.UploadFile(filePath))
+            {
+                var imageUrl = $"{urlBase}/{photo.FileName}";
+                var response = await CallAIHairstyleApi(imageUrl, prompt);
+                if (response != null)
+                {
+                    ViewData["ResultUrl"] = response.output;
+                }
+                ViewData["OriginalUrl"] = imageUrl;
+                return View("Result");
+            }
+            else
+            {
+                return BadRequest("Failed to upload the file");
+            }
+        }
+
+        private async Task<OrderStatus> CallAIHairstyleApi(string imageUrl, string prompt)
+        {
+
+            var requestData = new
+            {
+                imageUrl = imageUrl,
+                textPrompt = prompt
+            };
+
+            var content = new StringContent(JsonSerializer.Serialize(requestData), Encoding.UTF8, "application/json");
+
+            var response = await client.PostAsync(ApiUrl, content);
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                var jsonResponse = await response.Content.ReadAsStringAsync();
+                using JsonDocument document = JsonDocument.Parse(jsonResponse);
+
+                if (document.RootElement.TryGetProperty("body", out JsonElement bodyElement))
+                {
+                    OrderResponse res = JsonSerializer.Deserialize<OrderResponse>(bodyElement.GetRawText());
+                    if (res != null)
+                    {
+                        if (res.status == "init")
+                        {
+                            await Task.Delay(3000);
+                            int tries = 0;
+                            while (tries < 4)
+                            {
+                                tries++;
+                                OrderStatus statusRes = await CheckOrderStatus(res.orderId);
+                                if (statusRes != null)
+                                {
+                                    if (statusRes.status == "active")
+                                    {
+                                        return statusRes;
+                                    }
+                                }
+                                await Task.Delay(3000);
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    throw new JsonException("Response JSON does not contain a 'body' property");
+                }
+            }
+            return null;
+
+        }
+
+        private async Task<OrderStatus> CheckOrderStatus(string orderId)
+        {
+
+            var requestData = new
+            {
+                orderId = orderId
+            };
+
+            var content = new StringContent(JsonSerializer.Serialize(requestData), Encoding.UTF8, "application/json");
+            //content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+            var response = await client.PostAsync(statusApiUrl, content);
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                var jsonResponse = await response.Content.ReadAsStringAsync();
+                using JsonDocument document = JsonDocument.Parse(jsonResponse);
+
+                if (document.RootElement.TryGetProperty("body", out JsonElement bodyElement))
+                {
+                    return JsonSerializer.Deserialize<OrderStatus>(bodyElement.GetRawText());
+                }
+                else
+                {
+                    throw new JsonException("Response JSON does not contain a 'body' property");
+                }
+            }
+            return null;
+
+        }
+
+        public class OrderResponse
+        {
+            public string orderId { get; set; }
+            public int maxRetriesAllowed { get; set; }
+            public int avgResponseTimeInSec { get; set; }
+            public string status { get; set; }
+        }
+
+        public class OrderStatus
+        {
+            public string orderId { get; set; }
+            public string status { get; set; }
+            public string output { get; set; }
+        }
+    }
+}
---
+++ wep/Ftp.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Threading;
+using System.Threading.Tasks;
+using FluentFTP;
+
+namespace wep
+{
+    internal static class Ftp
+    {
+        static string host = "ftp.masri.uk";
+        static string username = "u519277213.dania";
+        static string password = "eX5/9BpZ;FR*N6GQ";
+        static string uploadPath = "/uploaded";
+        static public string successMessage = "File uploaded. URL copied to clipboard.";
+        static public string failMessage = "An error occoured while uploading image.";
+
+        public static bool UploadFile(string file)
+        {
+            string filename = System.IO.Path.GetFileName(file);
+            string upload = $"{uploadPath}/{filename}";
+            using (var ftp = new FtpClient(host, username, password))
+            {
+                ftp.Connect();
+                FtpStatus status = ftp.UploadFile(@file, upload, FtpRemoteExists.Overwrite, createRemoteDir: false);
+                switch (status)
+                {
+                    case FtpStatus.Success:
+                        return true;
+                    default:
+                        return false;
+                }
+            }
+        }
+
+        public static bool CreateDirectory()
+        {
+            bool res;
+            using (var conn = new FtpClient(host, username, password))
+            {
+                conn.Connect();
+                res = conn.CreateDirectory(uploadPath);
+            }
+            return res;
+        }
+
+        public static List<string> GetListing()
+        {
+
+            using (var conn = new FtpClient(host, username, password))
+            {
+                var res = new List<string>();
+                conn.Connect();
+
+                // get a recursive listing of the files & folders in a specific folder
+                foreach (var item in conn.GetListing("/", FtpListOption.Auto))
+                {
+                    string str = "";
+                    switch (item.Type)
+                    {
+
+                        case FtpObjectType.Directory:
+
+                            str = "Dir:  " + item.FullName + "Modified date:  " + conn.GetModifiedTime(item.FullName);
+
+                            break;
+
+                        case FtpObjectType.File:
+                            str = "File: " + item.FullName
+                                + "Modified date:  " + conn.GetModifiedTime(item.FullName)
+                                + "File size:  " + conn.GetFileSize(item.FullName)
+                                + "Chmod:  " + conn.GetChmod(item.FullName);
+
+                            break;
+
+                        case FtpObjectType.Link:
+                            str = "Link: " + item.FullName;
+                            break;
+                    }
+                    res.Add(str);
+                }
+                return res;
+            }
+        }
+
+
+    }
+}
